9b6d2b2bf535fbb89af523274dc4f41f00dc2380
d.savuljesku <d.savuljesku@gmail.com>
2024-04-26 13:33:34 +0200

    Improve text inspector
    [4.x]
    
    ERM35563
    
    Change-Id: Ibc27aeeebf70ace9d54b277757ea1a6e6e1a3177

--- resources/ext.visualEditorPlus.inlineTextInspector/bootstrap.js
+++ resources/ext.visualEditorPlus.inlineTextInspector/bootstrap.js
@@ -6,11 +6,14 @@ mw.hook( 've.activationComplete' ).add( function () {
 
 function registerInlineTextInspectors( target ) {
 	if ( target.getSurface().getMode() === 'visual' ) {
+		var $popupTarget = $( '<div>' );
+		$( '.ve-init-target' ).append( $popupTarget );
 		var inspectorPopup = new ext.visualEditorPlus.ui.InlineTextInspector( {
 			$overlay: true,
+			$container: $popupTarget
 		} );
 		inspectorPopup.setTarget( target );
-		$( document.body ).append( inspectorPopup.$element );
+		$popupTarget.append( inspectorPopup.$element );
 
 		for ( var key in ext.visualEditorPlus.registry.inlineTextInspectors.registry ) {
 			var cls = ext.visualEditorPlus.registry.inlineTextInspectors.registry[ key ],
