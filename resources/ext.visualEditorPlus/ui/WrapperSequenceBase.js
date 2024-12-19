1140f0f6445ce7c40608e7abe7b9a61a5cbd4899
Robert Vogel <vogel@hallowelt.biz>
2024-12-19 15:51:51 +0100

    Require whitespace before and after the wrapper
    This avoids odd auto-formattings, e.g. with Links with underscores.
    
    ERM38583
    
    [4.5.x]
    
    Change-Id: If9ff8f7365b7af90e1b9d80ace567adca91a38e2
    (cherry picked from commit 7df5cb71527559cf3b9899cc2573864b45d1adc9)
    (cherry picked from commit f523d56668dcf03e5f8901c4d603b386311e7be4)

--- resources/ext.visualEditorPlus/ui/WrapperSequenceBase.js
+++ resources/ext.visualEditorPlus/ui/WrapperSequenceBase.js
@@ -6,7 +6,7 @@ ext.visualEditorPlus.ui.WrapperSequenceBase = function ExtVisualEditorPlusUiWrap
 	ext.visualEditorPlus.ui.WrapperSequenceBase.super.apply( this, [
 		this.wrapperSequenceName,
 		this.wrapperCommandName,
-		new RegExp( this.wrapperWrapChar + '.*?' + this.wrapperWrapChar + '.' ),
+		new RegExp( '\\s' + this.wrapperWrapChar + '.*?' + this.wrapperWrapChar + '\\s' ),
 		undefined,
 		{
 			setSelection: true
@@ -16,22 +16,20 @@ ext.visualEditorPlus.ui.WrapperSequenceBase = function ExtVisualEditorPlusUiWrap
 OO.inheritClass( ext.visualEditorPlus.ui.WrapperSequenceBase, ve.ui.Sequence );
 
 ext.visualEditorPlus.ui.WrapperSequenceBase.prototype.execute = function ( surface, range ) {
-	var surfaceModel = surface.getModel();
-	var firstBackTickRange = new ve.Range( range.start, range.start + 1 );
-	var lastBackTickRange = new ve.Range( range.end - 2, range.end - 1 );
+	var surfaceModel = surface.getModel(),
+	firstWrapperRange = new ve.Range( range.start + 1, range.start + 2 ),
+	lastWrapperRange = new ve.Range( range.end - 3, range.end - 1 ),
 
-	var firstBackTickFragment = surfaceModel.getLinearFragment( firstBackTickRange, true, true );
-	var lastBackTickFragment = surfaceModel.getLinearFragment( lastBackTickRange, true, true );
+	firstWrapperFragment = surfaceModel.getLinearFragment( firstWrapperRange, true, true );
+	firstWrapperFragment.removeContent();
+	lastWrapperFragment = surfaceModel.getLinearFragment( lastWrapperRange, true, true );
+	lastWrapperFragment.removeContent();
 
-	firstBackTickFragment.removeContent();
-	lastBackTickFragment.removeContent();
-
-	range = new ve.Range( range.start, range.end - 3 );
-	var parentReturn = ext.visualEditorPlus.ui.WrapperSequenceBase.super.prototype.execute.apply( this, arguments );
-
-	var newSelection = surfaceModel.getSelection().collapseToEnd();
-	var newRange = newSelection.getRange();
-	var newFragment = surfaceModel.getLinearFragment( newRange, true, true );
+	range = new ve.Range( range.start + 1, range.end - 3 );
+	var parentReturn = ext.visualEditorPlus.ui.WrapperSequenceBase.super.prototype.execute.apply( this, arguments ),
+	newSelection = surfaceModel.getSelection().collapseToEnd(),
+	newRange = newSelection.getRange(),
+	newFragment = surfaceModel.getLinearFragment( newRange, true, true );
 
 	newFragment.insertContent( ' ' ).collapseToEnd().select();
 
