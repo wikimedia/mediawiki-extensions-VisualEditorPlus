b41f01b308674d0bd477e4dba6adb6a5ef747545
d.savuljesku <d.savuljesku@gmail.com>
2025-05-20 15:46:09 +0200

    Implement generic tag handling
    Move and refactor from BSVEC
    
    [5.1.x]
    
    ERM42208
    
    Change-Id: I30ff1004f0896f549c33b8b29dd66be8023c5d49
    (cherry picked from commit a11486f52c8cea159170ead2ce4efd1998c154e7)

--- resources/ext.visualEditorPlus.bootstrap.js
+++ resources/ext.visualEditorPlus.bootstrap.js
@@ -1,3 +1,6 @@
 window.ext = window.ext || {};
 window.ext.visualEditorPlus = window.ext.visualEditorPlus || {};
 window.ext.visualEditorPlus.ui = window.ext.visualEditorPlus.ui || {};
+window.ext.visualEditorPlus.ui.tag = window.ext.visualEditorPlus.ui.tag || {};
+window.ext.visualEditorPlus.ce = window.ext.visualEditorPlus.ce || {};
+window.ext.visualEditorPlus.dm = window.ext.visualEditorPlus.dm || {};
